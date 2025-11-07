222f651d9a3be3a49a844b71d73a6938b124c479
noxay <noxayo@proton.me>
2025-11-07 10:28:58 +0100

    Init dev branch and adding classes needed for the first milestone.

---
+++ src/main/java/org/example/Color.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Color {
+    double r, g, b;
+    public Color(double r, double g, double b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Point {
+
+    public Point() {
+
+
+
+    }
+
+}
