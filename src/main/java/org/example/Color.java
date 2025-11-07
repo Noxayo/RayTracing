5dbe3b3e0bd9cb791e1e68daa713cf051bdec6e4
noxay <noxayo@proton.me>
2025-11-07 10:58:00 +0100

    Point and color class

--- src/main/java/org/example/Color.java
+++ src/main/java/org/example/Color.java
@@ -1,15 +1,43 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Color {
-    double r, g, b;
+    private double r, g, b;
     public Color(double r, double g, double b) {
         this.r = r;
         this.g = g;
         this.b = b;
     }
+    public double getR() {
+        return r;
+    }
+    public void setR(double r) {
+        this.r = r;
+    }
+    public double getG() {
+        return g;
+    }
+    public void setG(double g) {
+        this.g = g;
+    }
+    public double getB() {
+        return b;
+    }
+    public void setB(double b) {
+        this.b = b;
+    }
+
 
     @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Color color = (Color) o;
+        return Double.compare(r, color.r) == 0 && Double.compare(g, color.g) == 0 && Double.compare(b, color.b) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(r, g, b);
     }
 }
