b7d24a563d84adc9704fab1a8269fa1a06a19fd4
ademp <ademtech@proton.me>
2025-12-09 17:46:18 +0100

    Résultat du Jalon 6 (Adem Side, ne pas prendre en compte)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -32,7 +32,7 @@ import java.util.Arrays;
 
             try {
                 // chemin de fichier .scene (j'ai toujours le message que le chemin d'accès marche pas)
-                String sceneFile = "src/main/java/Scenes/final_avec_bonus.scene";
+                String sceneFile = "src/main/java/Scenes/tp51-diffuse.test";
 
                 System.out.println(" Lecture du fichier: " + sceneFile);
                 System.out.println("...\n");
